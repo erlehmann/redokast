1ecc369ae13f87f9137ba0b6901b14b5816dc36e
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-12-02 20:33:58 +0100

    + WebVTT generation, now with more UTF-8

--- generate-webvtt.py
+++ generate-webvtt.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from codecs import getwriter
 from linklist import parse_linklist, serialize_webvtt
 from sys import argv, stdin, stdout
+stdout = getwriter('utf-8')(stdout)
 
 data = parse_linklist(stdin.read(), remove_tags=True)
 data = filter(lambda e: e['url'] == '', data)
